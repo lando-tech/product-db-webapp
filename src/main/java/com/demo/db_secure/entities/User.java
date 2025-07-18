52f4b8c15b1e148be2a58cd642576c0e96c6e5c6
aaron newman <aaronlnewman@gmail.com>
2025-07-18 14:40:47 +0900

    started userForm to enable adding new users, currently working through issues in the db entry table.

--- src/main/java/com/demo/db_secure/entities/User.java
+++ src/main/java/com/demo/db_secure/entities/User.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity(name = "User")
-@Table(name = "USER")
+@Table(name = "users")
 public class User implements Serializable {
 
     @Id
--- src/main/java/com/demo/db_secure/repositories/UserRepo.java
+++ src/main/java/com/demo/db_secure/repositories/UserRepo.java
@@ -9,21 +9,21 @@ import com.demo.db_secure.entities.User;
 
 
 public interface UserRepo extends CrudRepository<User, Long>{
-    @Query("SELECT p FROM User p WHERE p.userName LIKE %?1%")
+    @Query("SELECT p FROM User p WHERE p.userName = ?1")
     Optional<User> findByUserName(String userName);
 
-    @Query("SELECT p FROM User p WHERE p.firstName LIKE %?1%")
+    @Query("SELECT p FROM User p WHERE p.firstName = ?1")
     Optional<User> findByFirstName(String firstName);
 
-    @Query("SELECT p FROM User p WHERE p.lastName LIKE %?1%")
+    @Query("SELECT p FROM User p WHERE p.lastName = ?1")
     Optional<User> findByLastName(String lastName);
 
-    @Query("SELECT p FROM User p WHERE p.email LIKE %?1%")
+    @Query("SELECT p FROM User p WHERE p.email = ?1")
     Optional<User> findByEmail(String email);
 
-    @Query("SELECT p FROM User p WHERE p.phoneNumber LIKE %?1%")
+    @Query("SELECT p FROM User p WHERE p.phoneNumber = ?1")
     Optional<User> findByPhoneNumber(String phoneNumber);
 
-    @Query("SELECT p FROM User p WHERE p.companyName LIKE %?1%")
+    @Query("SELECT p FROM User p WHERE p.companyName = ?1")
     Optional<User> findByCompany(String companyName);
 }
